523d8bd2b87254a284dc4325b7a3b5d97da2e9f5
Mithila S Shegaji <113339980+MithilaShegaji@users.noreply.github.com>
2025-05-20 22:15:54 +0530

    Delete public directory

--- public/js/home.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', () => {
-    const video = document.getElementById('scroll-video');
-    const scrollPosition = window.scrollY;
-    const documentHeight = document.body.scrollHeight - window.innerHeight;
-
-    const scrollFraction = scrollPosition / documentHeight;
-    video.currentTime = scrollFraction * video.duration;
-});
